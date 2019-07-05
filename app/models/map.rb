e160b597dae6ec13e0a93f492dfc719e3b31c022
Eduardo Straub <eduardo.straub1@gmail.com>
2019-07-04 21:50:40 -0400

    se agrega el centro y boostrap 4.3.1

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,13 @@
 class Map < ApplicationRecord
+  before_create :add_remaining_data
   belongs_to :user
+
+  def add_remaining_data
+    add_center
+  end
+
+  def add_center
+    self.center[0] = self.latitude
+    self.center[1] = self.longitude
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :maps
   devise_for :users
+  root 'maps#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
